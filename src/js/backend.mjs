46395ac6324d3a30e4f370123bbef045826278ec
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2026-04-24 11:01:13 +0200

    maj caps

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -1,8 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const DEFAULT_POCKETBASE_URL = import.meta.env.DEV
-	? "http://127.0.0.1:8090"
-	: "https://pbportfolio.geiss-solene.fr/";
+const DEFAULT_POCKETBASE_URL = "https://pbportfolio.geiss-solene.fr/";
 
 const POCKETBASE_URL = import.meta.env.PUBLIC_POCKETBASE_URL || DEFAULT_POCKETBASE_URL;
 const db = new PocketBase(POCKETBASE_URL);
